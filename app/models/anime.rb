5e33856bcc96863797da6f8c0eb286bee14dd63a
中島慎二 <nakajimashinji@nakajimashinjinoMacBook-Air.local>
2023-06-15 16:29:03 +0900

    レスポンシブ完了

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -2,7 +2,7 @@ class Anime < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :title, presence: true, length: { maximum: 30 }
+  validates :title, presence: true, length: { maximum: 10 }
   validates :all_part, presence: true, numericality: { only_integer: true }
   validates :current_part, presence: true, numericality: { only_integer: true, less_than_or_equal_to: :all_part }
 end
